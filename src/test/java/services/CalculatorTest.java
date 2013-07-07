aa76658e1f3c4ade3d38c29b4a7cd3a64c4bd4ed
hase502 <hase502@hase502-PC>
2013-07-07 13:46:06 +0900

    テストケース追加

--- src/test/java/services/CalculatorTest.java
+++ src/test/java/services/CalculatorTest.java
@@ -13,4 +13,11 @@ public class CalculatorTest {
         String actual = sut.add(1, 1);
         assertThat(actual, is("2"));
     }
+    
+    @Test
+    public void testAdd2() {
+        Calculator sut = new Calculator();
+        String actual = sut.add(1, 3);
+        assertThat(actual, is("4"));
+    }
 }
